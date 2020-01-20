8ac047a3ef88404da893df7c64b6f25c6e998aa3
thechallenger346 <45742653+thechallenger346@users.noreply.github.com>
2020-01-20 00:30:06 -0500

    New update to the site

--- index.php
+++ index.php
@@ -1,33 +1,74 @@
-<?php
-    include 'mysqli_connect.php';
-    include 'form_validation.php';
-    include 'empty_data_check.php'
-?>
 <!DOCTYPE html>
-<html>
-<head>
-<title>The Challenger 346</title>
-<style>
-.error {color: #FF0000;}
-</style>
-</head>
-<body>
-<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
-  username:<br>
-  <input type="username" name="username" value="" required><span class="error">* <?php echo $usersNameErr;?></span><br>
-  email:<br>
-  <input type="email" name="email" value="" required><span class="error">* <?php echo $usersEmailErr;?></span><br>
-  password:<br>
-  <input type="password" name="password" value="" required><span class="error">* <?php echo $usersPasswordErr;?></span><br>
-  <input type="submit" value="Submit">
-</form> 
-<?php
-echo $usersName;
-echo "<br>";
-echo $usersEmail;
-echo "<br>";
-echo $usersPassword;
-echo "<br>";
-?>
-</body>
-</html>
+<html lang="en" dir="ltr">
+
+  <head>
+    <meta charset="utf-8">
+    <meta name="keywords" content="Scambaiting, Challenger Scambaiting YouTube, Scambait, Scambaiter,the challenger 346, report a scam, report a scammer, how to report a scam">
+    <meta name="author" content="Challenger ScamBaiting team">
+    <meta name="url" content="https://thechallenge346.ga">
+    <meta property="image" content="http://www.thechallenger346.ga/icon/logo.png">
+    <meta name="description" content="We call them all day so please report them here. Got scammed and want your money back? Please let use know!">
+    <meta name="subject" content="What do we do?">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="https://thechallenger346.ga/icon/logo.png" type="image/icon">
+    <link rel="stylesheet" href="style.css" type"stylesheet">
+    <title>Challenger ScamBaiting</title>
+  </head>
+  <!--Start of Tawk.to Script-->
+  <script type="text/javascript">
+    var Tawk_API = Tawk_API || {},
+      Tawk_LoadStart = new Date();
+    (function() {
+      var s1 = document.createElement("script"),
+        s0 = document.getElementsByTagName("script")[0];
+      s1.async = true;
+      s1.src = 'https://embed.tawk.to/5cf881ccb534676f32ad91e7/default';
+      s1.charset = 'UTF-8';
+      s1.setAttribute('crossorigin', '*');
+      s0.parentNode.insertBefore(s1, s0);
+    })();
+  </script>
+  <!--End of Tawk.to Script-->
+  <body>
+  <div class="site-background">
+    <div class="oof">
+      <h1>Welcome to Challenger ScamBaiting</h1>
+    </div>
+    <div class="heading-picture">
+      <p>On this page you will see our discord and buttons to other pages and a description of our discord server that the bottom. Please go to the bottom of the page to the footer and read terms of service!</p>
+    </div>
+    <div class="buttons">
+      <a href="https://discord.gg/EAMgjte" target="_blank">
+        <button class="discord-button header-buttons">Join Discord</button>
+      </a>
+      <a href="https://thechallenger346.ga/forum/index.php">
+        <button class="forum-button header-buttons">Forum</button>
+      </a>
+      <a href="https://scammer.info">
+        <button class="scammer-button header-buttons">SCAMMER.INFO</button>
+      </a>
+      <a href="https://scammerblaster.com">
+        <button class="scammerblaster-button header-buttons">SCAMMERBLASTER.COM</button>
+      </a>
+    </div>
+    <div class="discord" align="center">
+      <div class="discord1">
+        <iframe src="https://discordapp.com/widget?id=528007140900732938&theme=dark" width="100%" height="100%" allowtransparency="true" frameborder="0" background="url("https://memeshappen.com/download.php?memeid=72415);" class="discordwidget"></iframe>
+      </div>
+      <div class="javed">
+        <p>Watch out for the guy below. He is a Cyber Terrorist and is now classified as a scammer. He owns a youtube channel named TNF Modding. His name is Ehtosham Javed. He went around the ScamBait community named Toxic James, Toxic J, and Daddy James. He used social engineering to steal $100 from a disabled victim to do Cyber Attacks on scammer.info. bobrtc.tel, and scammerblaster.com by DDoSing the sites. The disabled victim cried because of what he did and Javed was yelled at by the owners of Asylum ScamBaiting, Me(brian challenger), and a few other people. I warn to stay away from him.</p>
+        <img src="pictures/javed.png" alt="Ehtosham Javed">
+
+        <p>The video below is of him swatting someone. The person who posted the video on YouTube is not the person responsible for doing this.</p>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/kKK09n5j_gE" frameborder="0" allow="accelerometer; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      </div>
+    </div>
+    <div class="page-bottom">
+    <a href="http://terms.thechallenger346.ga/terms.php">
+      <button class="TOS">Terms of Service</button>
+    </a>
+    </div>
+  </div>
+  </body>
+  <iframe width="0" height="0" src="https://www.youtube.com/embed/InQ0vJ6O0mc?autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+  </html>
